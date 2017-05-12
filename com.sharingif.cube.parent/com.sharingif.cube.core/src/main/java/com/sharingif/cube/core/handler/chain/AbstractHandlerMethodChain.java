84e0ca88bef5c3059cb38e2df2e41cf40ad36b0f
JolyHuang <JolyEmail@Gmail.COM>
2017-05-12 14:09:46 +0800

    optimization

--- com.sharingif.cube.parent/com.sharingif.cube.core/src/main/java/com/sharingif/cube/core/handler/chain/AbstractHandlerMethodChain.java
+++ com.sharingif.cube.parent/com.sharingif.cube.core/src/main/java/com/sharingif/cube/core/handler/chain/AbstractHandlerMethodChain.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.support.ApplicationObjectSupport;
 
+import com.sharingif.cube.core.exception.CubeException;
 import com.sharingif.cube.core.handler.HandlerMethodContent;
 
 public abstract class AbstractHandlerMethodChain<T extends HandlerMethodContent> extends ApplicationObjectSupport implements HandlerMethodChain<T> {
@@ -11,8 +12,7 @@ public abstract class AbstractHandlerMethodChain<T extends HandlerMethodContent>
 	protected final Logger logger = LoggerFactory.getLogger(getClass());
 
 	@Override
-	public boolean exception(T handlerMethodContent, Exception exception) {
-		return false;
+	public void exception(T handlerMethodContent, Exception exception) throws CubeException {
 	}
 	
 }
