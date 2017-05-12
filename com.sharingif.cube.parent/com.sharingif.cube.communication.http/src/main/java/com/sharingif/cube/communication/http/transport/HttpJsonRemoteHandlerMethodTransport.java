fb091a7cdf9b3558b349f8063b173fd654dd062c
JolyHuang <JolyEmail@Gmail.COM>
2017-05-12 13:57:19 +0800

    optimization

--- com.sharingif.cube.parent/com.sharingif.cube.communication.http/src/main/java/com/sharingif/cube/communication/http/transport/HttpJsonRemoteHandlerMethodTransport.java
+++ com.sharingif.cube.parent/com.sharingif.cube.communication.http/src/main/java/com/sharingif/cube/communication/http/transport/HttpJsonRemoteHandlerMethodTransport.java
@@ -1,31 +1,19 @@
 package com.sharingif.cube.communication.http.transport;
 
-import java.lang.annotation.Annotation;
-import java.util.Map;
-
-import org.springframework.beans.BeanUtils;
-import org.springframework.beans.MutablePropertyValues;
-import org.springframework.core.Conventions;
-import org.springframework.core.MethodParameter;
-import org.springframework.core.annotation.AnnotationUtils;
-import org.springframework.validation.DataBinder;
-import org.springframework.validation.Errors;
-import org.springframework.validation.annotation.Validated;
-
 import com.sharingif.cube.communication.JsonModel;
 import com.sharingif.cube.communication.exception.BusinessCommunicationException;
+import com.sharingif.cube.communication.exception.CommunicationException;
 import com.sharingif.cube.communication.transport.AbstractRemoteHandlerMethodTransport;
 import com.sharingif.cube.communication.transport.Connection;
+import com.sharingif.cube.communication.transport.transform.MethodParameterArgument;
 import com.sharingif.cube.communication.transport.transform.Transform;
 import com.sharingif.cube.communication.transport.transform.exception.MarshallerException;
 import com.sharingif.cube.core.exception.CubeException;
-import com.sharingif.cube.core.exception.CubeRuntimeException;
-import com.sharingif.cube.core.exception.validation.BindValidationCubeException;
 import com.sharingif.cube.core.handler.HandlerMethod;
 import com.sharingif.cube.core.handler.HandlerMethodContent;
-import com.sharingif.cube.core.handler.bind.support.DataBinderFactory;
 import com.sharingif.cube.core.request.RequestInfo;
 import com.sharingif.cube.core.util.CubeExceptionUtil;
+import com.sharingif.cube.core.util.StringUtils;
 
 /**
  * 提供远程方法访问
@@ -34,10 +22,10 @@ import com.sharingif.cube.core.util.CubeExceptionUtil;
  * @version v1.0
  * @since v1.0
  */
-public class HttpJsonRemoteHandlerMethodTransport extends AbstractRemoteHandlerMethodTransport<RequestInfo<Object[]>,Object[],String,String,JsonModel<Map<String, Object>>,RequestInfo<String>,String> {
+public class HttpJsonRemoteHandlerMethodTransport extends AbstractRemoteHandlerMethodTransport<RequestInfo<Object[]>,Object[],String,MethodParameterArgument<Object[],String>,JsonModel<Object>,RequestInfo<String>,String> {
 	
 	private Connection<RequestInfo<String>,String> connection;
-	private Transform<Object[],String, String, JsonModel<Map<String, Object>>> transform;
+	private Transform<Object[],String, MethodParameterArgument<Object[],String>, JsonModel<Object>> transform;
 
 	public HttpJsonRemoteHandlerMethodTransport(HandlerMethod handlerMethod) {
 		super(handlerMethod);
@@ -47,90 +35,83 @@ public class HttpJsonRemoteHandlerMethodTransport extends AbstractRemoteHandlerM
 		this.connection = connection;
 	}
 	
-	public void setTransform(Transform<Object[], String, String, JsonModel<Map<String, Object>>> transform) {
+	public void setTransform(Transform<Object[], String, MethodParameterArgument<Object[],String>, JsonModel<Object>> transform) {
 		this.transform = transform;
 	}
 
 	@Override
-	public Object doTransport(RequestInfo<Object[]> requestInfo) {
+	public Object doTransport(RequestInfo<Object[]> requestInfo) throws CubeException {
 		
 		Object returnValue = null;
-		boolean handlerMethodChainIsEmpty = (null != getHandlerMethodChain());
+		
+		boolean handlerMethodChainIsNotEmpty = (null != getHandlerMethodChain());
 		HandlerMethodContent handlerMethodContent = null;
+		
+		String jsonData = marshaller(requestInfo);
+		
+		if(handlerMethodChainIsNotEmpty) {
+			handlerMethodContent = new HandlerMethodContent(getBean(), getMethod(), requestInfo.getRequest(), null, getMethodParameters(), requestInfo.getLocale(), requestInfo);
+			getHandlerMethodChain().before(handlerMethodContent);
+		}
 		try {
-			if(handlerMethodChainIsEmpty) {
-				handlerMethodContent = new HandlerMethodContent(getBean(), getMethod(), requestInfo.getRequest(), null, getMethodParameters(), requestInfo.getLocale(), requestInfo);
-				getHandlerMethodChain().before(handlerMethodContent);
-			}
 			
-			RequestInfo<String> httpJsonContext = null;
-			try {
-				String jsonData = transform.marshaller(requestInfo.getRequest());
-				
-				httpJsonContext = new RequestInfo<String>(requestInfo.getMediaType(), requestInfo.getLookupPath(), requestInfo.getLocale(), requestInfo.getMethod(), jsonData);
-			} catch (MarshallerException e) {
-				throw new CubeRuntimeException(e.getMessage(),e);
-			}
+			String connectReturnValue = connect(requestInfo, jsonData);
 			
-			String resultData = connection.connect(httpJsonContext);
+			JsonModel<Object> jsonModel = unmarshaller(connectReturnValue, requestInfo);
 			
-			JsonModel<Map<String, Object>> jsonModel = null;
-			try {
-				jsonModel = transform.unmarshaller(resultData);
-			} catch (MarshallerException e) {
-				throw new CubeRuntimeException(e.getMessage(),e);
-			}
+			handleException(jsonModel);
 			
-			exceptionHandler(jsonModel);
+			returnValue = jsonModel.get_data();
 			
-			Map<String,Object> dataMap = jsonModel.get_data();
-			
-			MethodParameter parameter = getReturnType();
-			
-			if(dataMap == null || parameter.getMethod().getReturnType().getName().equals(Void.TYPE.getName()) ) {
-				return null;
-			}
-			
-			DataBinder binder = null;
-			try {
-				String name = Conventions.getVariableNameForParameter(parameter);
-				Object attribute = createAttribute(name, parameter, requestInfo, getDataBinderFactory());
-				binder = getDataBinderFactory().createBinder(requestInfo, attribute, name);
-			} catch (CubeException e) {
-				throw new CubeRuntimeException("data binder error", e);
-			}
-			if (binder.getTarget() != null) {
-				bindRequestParameters(binder, requestInfo, dataMap);
-				validateIfApplicable(binder, parameter);
-				if (binder.getBindingResult().hasErrors() && isBindExceptionRequired(binder, parameter)) {
-					throw new BindValidationCubeException(binder.getBindingResult());
-				}
-			}
-			
-			returnValue = binder.convertIfNecessary(binder.getTarget(), parameter.getParameterType(), parameter);
-			if(handlerMethodChainIsEmpty) {
-				handlerMethodContent.setReturnValue(returnValue);
-				getHandlerMethodChain().after(handlerMethodContent);
-			}
 		} catch (Exception exception) {
-			if(handlerMethodChainIsEmpty) {
-				boolean flag = getHandlerMethodChain().exception(handlerMethodContent, exception);
-				if(!flag) {
-					CubeExceptionUtil.throwCubeRuntimeException(exception);
-				} 
-			} else {
-				CubeExceptionUtil.throwCubeRuntimeException(exception);
+			if(handlerMethodChainIsNotEmpty) {
+				try {
+					getHandlerMethodChain().exception(handlerMethodContent, exception);
+				} catch (Exception handlerMethodChainException) {
+					this.logger.error("doTransport error", exception);
+					CubeExceptionUtil.throwCubeRuntimeException(handlerMethodChainException);
+				}
+				
 			}
+			
+			CubeExceptionUtil.throwCubeRuntimeException(exception);
+		}
+			
+		if(handlerMethodChainIsNotEmpty) {
+			handlerMethodContent.setReturnValue(returnValue);
+			getHandlerMethodChain().after(handlerMethodContent);
 		}
 		
-		if(handlerMethodChainIsEmpty) {
+		if(handlerMethodChainIsNotEmpty) {
 			return handlerMethodContent.getReturnValue();
 		} else {
 			return returnValue;
 		}
 	}
 	
-	protected void exceptionHandler(JsonModel<Map<String, Object>> jsonModel) throws BusinessCommunicationException {
+	protected JsonModel<Object> unmarshaller(String connectReturnValue, RequestInfo<Object[]> requestInfo) throws MarshallerException {
+		MethodParameterArgument<Object[], String> methodParameterArgument = new MethodParameterArgument<Object[], String>(getReturnType(), requestInfo, getDataBinderFactory(), connectReturnValue);
+		
+		return transform.unmarshaller(methodParameterArgument);
+	}
+	
+	protected String marshaller(RequestInfo<Object[]> requestInfo) throws MarshallerException {
+		return transform.marshaller(requestInfo.getRequest());
+	}
+	
+	protected String connect(RequestInfo<Object[]> requestInfo, String jsonData) {
+		RequestInfo<String> httpJsonContext = new RequestInfo<String>(requestInfo.getMediaType(), requestInfo.getLookupPath(), requestInfo.getLocale(), requestInfo.getMethod(), jsonData);
+		
+		String connectReturnValue = connection.connect(httpJsonContext);
+		
+		if(StringUtils.isEmpty(connectReturnValue) || "".equals(connectReturnValue.trim())) {
+			throw new CommunicationException("The reponse message is empty");
+		}
+		
+		return connectReturnValue;
+	}
+	
+	protected void handleException(JsonModel<Object> jsonModel) throws BusinessCommunicationException {
 		if(!jsonModel.get_tranStatus()) {
 			BusinessCommunicationException businessCommunicationException = new BusinessCommunicationException(jsonModel.get_exceptionMessage());
 			businessCommunicationException.setLocalizedMessage(jsonModel.get_exceptionLocalizedMessage());
@@ -139,62 +120,4 @@ public class HttpJsonRemoteHandlerMethodTransport extends AbstractRemoteHandlerM
 		}
 	}
 	
-	/**
-	 * Extension point to bind the request to the target object.
-	 * @param binder the data binder instance to use for the binding
-	 * @param request the current request
-	 */
-	protected void bindRequestParameters(DataBinder binder, RequestInfo<?> requestInfo, Map<?,?> parameter) {
-		MutablePropertyValues mpvs = new MutablePropertyValues(parameter);
-		binder.bind(mpvs);
-	}
-	
-	/**
-	 * Validate the model attribute if applicable.
-	 * <p>The default implementation checks for {@code @javax.validation.Valid},
-	 * Spring's {@link org.springframework.validation.annotation.Validated},
-	 * and custom annotations whose name starts with "Valid".
-	 * @param binder the DataBinder to be used
-	 * @param methodParam the method parameter
-	 */
-	protected void validateIfApplicable(DataBinder binder, MethodParameter methodParam) {
-		Annotation[] annotations = methodParam.getParameterAnnotations();
-		for (Annotation ann : annotations) {
-			Validated validatedAnn = AnnotationUtils.getAnnotation(ann, Validated.class);
-			if (validatedAnn != null || ann.annotationType().getSimpleName().startsWith("Valid")) {
-				Object hints = (validatedAnn != null ? validatedAnn.value() : AnnotationUtils.getValue(ann));
-				Object[] validationHints = (hints instanceof Object[] ? (Object[]) hints : new Object[] {hints});
-				binder.validate(validationHints);
-				break;
-			}
-		}
-	}
-	
-	/**
-	 * Extension point to create the model attribute if not found in the model.
-	 * The default implementation uses the default constructor.
-	 * @param attributeName the name of the attribute (never {@code null})
-	 * @param methodParam the method parameter
-	 * @param binderFactory for creating WebDataBinder instance
-	 * @param request the current request
-	 * @return the created model attribute (never {@code null})
-	 */
-	protected Object createAttribute(String attributeName, MethodParameter methodParam, RequestInfo<?> requestInfo, DataBinderFactory dataBinderFactory) throws CubeException {
-
-		return BeanUtils.instantiateClass(methodParam.getParameterType());
-	}
-	
-	/**
-	 * Whether to raise a fatal bind exception on validation errors.
-	 * @param binder the data binder used to perform data binding
-	 * @param methodParam the method argument
-	 * @return {@code true} if the next method argument is not of type {@link Errors}
-	 */
-	protected boolean isBindExceptionRequired(DataBinder binder, MethodParameter methodParam) {
-		int i = methodParam.getParameterIndex();
-		Class<?>[] paramTypes = methodParam.getMethod().getParameterTypes();
-		boolean hasBindingResult = (paramTypes.length > (i + 1) && Errors.class.isAssignableFrom(paramTypes[i + 1]));
-		return !hasBindingResult;
-	}
-
 }
