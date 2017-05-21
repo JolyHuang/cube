f08bfdb6b7e562403257ee52bf43fc64d9619543
JolyHuang <JolyEmail@Gmail.COM>
2017-05-21 16:40:57 +0800

    optimization

--- com.sharingif.cube.parent/com.sharingif.cube.web.vert.x/src/main/java/com/sharingif/cube/web/vert/x/handler/chain/VertXDispatcherHandlerExceptionChain.java
+++ com.sharingif.cube.parent/com.sharingif.cube.web.vert.x/src/main/java/com/sharingif/cube/web/vert/x/handler/chain/VertXDispatcherHandlerExceptionChain.java
@@ -1,8 +1,8 @@
 package com.sharingif.cube.web.vert.x.handler.chain;
 
 import com.sharingif.cube.communication.MediaType;
+import com.sharingif.cube.communication.http.handler.HttpHandlerMethodContent;
 import com.sharingif.cube.core.exception.CubeException;
-import com.sharingif.cube.core.handler.HandlerMethodContent;
 import com.sharingif.cube.core.handler.chain.AbstractHandlerMethodChain;
 import com.sharingif.cube.core.request.RequestInfo;
 import io.vertx.core.http.HttpHeaders;
@@ -16,21 +16,21 @@ import io.vertx.ext.web.RoutingContext;
  * @version v1.0
  * @since v1.0
  */
-public class VertXDispatcherHandlerExceptionChain extends AbstractHandlerMethodChain<HandlerMethodContent> {
+public class VertXDispatcherHandlerExceptionChain extends AbstractHandlerMethodChain<HttpHandlerMethodContent> {
 
     @Override
-    public void before(HandlerMethodContent handlerMethodContent) throws CubeException {
+    public void before(HttpHandlerMethodContent handlerMethodContent) throws CubeException {
 
     }
 
     @Override
-    public void after(HandlerMethodContent handlerMethodContent) throws CubeException {
+    public void after(HttpHandlerMethodContent handlerMethodContent) throws CubeException {
 
     }
 
     @SuppressWarnings("unchecked")
 	@Override
-    public void exception(HandlerMethodContent content, Exception exception) throws CubeException {
+    public void exception(HttpHandlerMethodContent content, Exception exception) throws CubeException {
         RequestInfo<RoutingContext> requestInfo = (RequestInfo<RoutingContext>) content.getRequestInfo();
 
         requestInfo.getRequest().response()
