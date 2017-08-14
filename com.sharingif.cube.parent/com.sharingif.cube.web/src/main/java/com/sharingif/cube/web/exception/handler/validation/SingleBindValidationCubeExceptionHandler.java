35f24bc2609786bd09f66022b0180743d3425203
JolyHuang <JolyEmail@Gmail.COM>
2017-08-14 15:36:35 +0800

    添加单一错误消息处理

---
+++ com.sharingif.cube.parent/com.sharingif.cube.web/src/main/java/com/sharingif/cube/web/exception/handler/validation/SingleBindValidationCubeExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.sharingif.cube.web.exception.handler.validation;
+
+import com.sharingif.cube.core.exception.ICubeException;
+import com.sharingif.cube.core.exception.validation.BindValidationCubeException;
+import org.springframework.validation.FieldError;
+
+import java.util.List;
+import java.util.Locale;
+
+/**
+ *
+ * 只返回一个错误信息
+ * @author Joly
+ * @version v1.0
+ * @since v1.0
+ * 2017/8/14 下午2:30
+ */
+public class SingleBindValidationCubeExceptionHandler extends BindValidationCubeExceptionHandler {
+
+    @Override
+    public void resolverMessages(ICubeException cubeException, Locale locale) {
+        super.resolverMessages(cubeException, locale);
+
+        BindValidationCubeException bindValidationCubeException = (BindValidationCubeException)cubeException;
+        List<FieldError> localeFieldErrors = bindValidationCubeException.getLocaleFieldErrors();
+
+        cubeException.setLocalizedMessage(localeFieldErrors.get(0).getDefaultMessage());
+    }
+
+}
