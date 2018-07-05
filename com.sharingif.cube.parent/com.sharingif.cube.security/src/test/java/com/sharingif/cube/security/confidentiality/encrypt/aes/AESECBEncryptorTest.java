97ea41cde82baba56cac4378269d5dbb2f9d8a17
JolyHuang <JolyEmail@Gmail.COM>
2018-07-05 22:20:35 +0800

    init

--- com.sharingif.cube.parent/com.sharingif.cube.security/src/test/java/com/sharingif/cube/security/confidentiality/encrypt/aes/AESECBEncryptorTest.java
+++ com.sharingif.cube.parent/com.sharingif.cube.security/src/test/java/com/sharingif/cube/security/confidentiality/encrypt/aes/AESECBEncryptorTest.java
@@ -64,10 +64,9 @@ public class AESECBEncryptorTest {
 	@Test
 	public void test() throws UnsupportedEncodingException {
 		Base64Coder base64Coder = new Base64Coder();
-		byte[] keysByte = base64Coder.decode("5+5u1xAgI9VnoO5R/8rU7oirbiudRggkL2e8bPgu1g0=");
+		byte[] keysByte = base64Coder.decode("3QSs80DfrozJ1j6Wt0IS5ICqzsSQHF3oLbbSg1qtXdE=");
 		AESECBEncryptor encryptor = new AESECBEncryptor(keysByte, base64Coder);
-		System.out.println(encryptor.encrypt("r3[vs5Utmk6Iceg6"));
-		System.out.println(encryptor.decrypt("QlroHFybYnzi0gz1zOPY0fl4tduFyfcV7P5fJMl8FOU="));
+		System.out.println(encryptor.encrypt("jfYYWE!*&NCs437923"));
 	}
 
 }
